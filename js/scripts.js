33cd5a5712e4f8dfcdeefa0331de11120bdc862c
jacob ruleaux, Michelle Poterek and Nicole Freed <jacob@>
2017-06-19 12:05:26 -0700

    add JS, JQuery, and CSS to compare inputted value to answer, change box border color (1 box)

--- js/scripts.js
+++ js/scripts.js
@@ -1,63 +1,96 @@
-function Square(answer,squareSize) {
-  this.answer = answer;
+var text1, text2, text3, text4, text5, text6, text7, text8, text9;
+var boxes3 = [text1, text2, text3, text4, text5, text6, text7, text8, text9];
+var answers3 = [8, 1, 6, 3, 5, 7, 4, 9, 2];
+
+function Square(squareSize, solved) {
+  this.boxes = boxes3;
+  // this.answer = answer;
   this.squareSize = squareSize;
   this.solved = solved;
 };
 
-function Box(empty, userInput) {
+function Box(empty, val, answer) {
   this.empty = empty;
-  this.userInput = userInput;
-}
-
-answers3 = [8, 1, 6, 3, 5, 7, 4, 9, 2];
+  this.val = val;
+  this.answer = answer;
+};
 
 // answers3 = {text1: 8, text2: 1, text3: 6, text4: 3, text5: 5, text6: 7, text7: 4, text8: 9, text9: 2};
+
+var box1 = new Box(true, text1, 8);
+var box2 = new Box(true, text2, 1);
+var box3 = new Box(true, text3, 6);
+var box4 = new Box(true, text4, 3);
+var box5 = new Box(true, text5, 5);
+var box6 = new Box(true, text6, 7);
+var box7 = new Box(true, text7, 4);
+var box8 = new Box(true, text8, 9);
+var box9 = new Box(true, text9, 2);
+
+function isCorrect(_val, _answer) {
+  if (_val === _answer) {
+    return true;
+  } else {
+    return false;
+  };
+};
 
 
 /////////////////////line in the sand/////////////////////////
 $(document).ready(function() {
+  var text1, text2, text3, text4, text5, text6, text7, text8, text9;
+
   $("#form1").submit(function(event) {
     event.preventDefault();
-    var text1 = $("input#text1").val();
+    text1 = parseInt($("input#text1").val());
+    console.log(text1);
+    console.log(box1.answer);
+    if (isCorrect(text1, box1.answer)) {
+      console.log(isCorrect(text1, box1.answer));
+      $("td#cell1").addClass("right");  // change box border to green
+    } else {
+      console.log(isCorrect(text1, box1.answer));
+      $("td#cell1").addClass("wrong");  // change box border to red
+    };
   });
 
   $("#form2").submit(function(event) {
     event.preventDefault();
-    var text2 = $("input#text2").val();
+    text2 = parseInt($("input#text2").val());
   });
 
   $("#form3").submit(function(event) {
     event.preventDefault();
-    var text3 = $("input#text3").val();
+    text3 = parseInt($("input#text3").val());
   });
 
   $("#form4").submit(function(event) {
     event.preventDefault();
-    var text4 = $("input#text4").val();
+    text4 = parseInt($("input#text4").val());
   });
 
   $("#form5").submit(function(event) {
     event.preventDefault();
-    var text5 = $("input#text5").val();
+    text5 = parseInt($("input#text5").val());
   });
 
   $("#form6").submit(function(event) {
     event.preventDefault();
-    var text6 = $("input#text6").val();
+    text6 = parseInt($("input#text6").val());
   });
 
   $("#form7").submit(function(event) {
     event.preventDefault();
-    var text7 = $("input#text7").val();
+    text7 = parseInt($("input#text7").val());
   });
 
   $("#form8").submit(function(event) {
     event.preventDefault();
-    var text8 = $("input#text8").val();
+    text8 = parseInt($("input#text8").val());
   });
 
   $("#form9").submit(function(event) {
     event.preventDefault();
-    var text9 = $("input#text9").val();
+    text9 = parseInt($("input#text9").val());
   });
 });
